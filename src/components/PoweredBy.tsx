a71206d4930cf1e3d38d03f1a34616f089fcba8a
Defi Launch <156188208+DefiLaunchApp@users.noreply.github.com>
2024-02-17 09:19:55 +0000

    Base Degen NFT Series 1

--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -4,7 +4,6 @@ export const PoweredBy: React.FC = () => {
       onClick={() => {
         window.open("https://thirdweb.com?utm_source=embed", "_blank");
       }}
-      className="spacing-1 absolute bottom-4 right-4 flex cursor-pointer items-center"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
